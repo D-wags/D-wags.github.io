86ec87d733b538ce51d6165fc2bbce3ec6f99956
Drew Wagner <drew_wagner@utexas.edu>
2017-04-03 23:48:48 -0500

    sorting works

--- assets/js/brands.js
+++ assets/js/brands.js
@@ -1,5 +1,5 @@
  // function sorts hot sauce brand names alphabetically
- function sortTable() {
+ function sortTable(ascending, indx) {
       var rows, i, x, y, shouldSwitch;
       var table = document.getElementById("brandTable");
       var switching = true;
@@ -10,10 +10,17 @@
         for (i = 1; i < (rows.length - 1); i++) {
           shouldSwitch = false;
          
-          x = rows[i].getElementsByTagName("TD")[0];
-          y = rows[i + 1].getElementsByTagName("TD")[0];
+          x = rows[i].getElementsByTagName("TD")[indx];
+          y = rows[i + 1].getElementsByTagName("TD")[indx];
+
+          if ((x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) && (ascending == true)) {
+       
+            shouldSwitch= true;
+            break;
+          }
+        
           
-          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          if ((x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) && (ascending == false)) {
        
             shouldSwitch= true;
             break;
@@ -27,7 +34,55 @@
     }
   }
 
-  $("#brandSorter").click(function() {
-    sortTable()
+  $("#asc-brand-sort").click(function() {
+    sortTable(true, 0)
+  });
+
+  $("#desc-brand-sort").click(function() {
+    sortTable(false, 0)
+  });
+
+  $("#asc-sauce-sort").click(function() {
+    sortTable(true, 0)
+  });
+
+  $("#desc-sauce-sort").click(function() {
+    sortTable(false, 0)
+  });
+
+
+  $("#asc-logo-sort").click(function() {
+    sortTable(true, 0)
+  });
+
+  $("#desc-logo-sort").click(function() {
+    sortTable(false, 0)
+  });
+
+
+   $("#asc-founder-sort").click(function() {
+    sortTable(true, 1)
+  });
+
+  $("#desc-founder-sort").click(function() {
+    sortTable(false, 1)
+  });
+
+
+   $("#asc-other-sort").click(function() {
+    sortTable(true, 3)
+  });
+
+  $("#desc-other-sort").click(function() {
+    sortTable(false, 3)
+  });
+
+
+   $("#asc-address-sort").click(function() {
+    sortTable(true, 4)
+  });
+
+  $("#desc-address-sort").click(function() {
+    sortTable(false, 4)
   });
 
--- assets/js/hotsauce.js
+++ assets/js/hotsauce.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
 
 
 //Function will sort by ascending scoville
-    function sortTable() {
+    function sortTablebyNum(ascending) {
       var rows, i, pep1, pep2, shouldSwitch;
       var table = document.getElementById("sauceTable");
       var switching = true;
@@ -43,7 +43,14 @@ $(document).ready(function() {
           pep2 = rows[i + 1].getElementsByTagName("TD")[2].getAttribute('data-sort');
 
           
-          if (parseInt(pep1) < parseInt(pep2)) {
+          if ((parseInt(pep1) > parseInt(pep2)) && (ascending==true)) {
+            
+            shouldSwitch = true;
+            break;
+          }
+          
+          if ((parseInt(pep1) < parseInt(pep2)) && (ascending==false)) {
+            
             shouldSwitch = true;
             break;
           }
@@ -56,8 +63,121 @@ $(document).ready(function() {
     }
   }
 
-  // on-click sort function call
-  $("#hotsauceSorter").click(function() {
-    console.log("get $");
-    sortTable();
+  //     function sortTablebyNum(ascending) {
+  //     var rows, i, pep1, pep2, shouldSwitch;
+  //     var table = document.getElementById("sauceTable");
+  //     var switching = true;
+      
+  //     while (switching) {
+  //       switching = false;
+  //       rows = table.getElementsByTagName("TR");
+  //       for (i = 1; i < (rows.length - 1); i++) {
+          
+  //         shouldSwitch = false;
+          
+  //         pep1 = rows[i].getElementsByTagName("TD")[2].getAttribute('data-sort');
+  //         pep2 = rows[i + 1].getElementsByTagName("TD")[2].getAttribute('data-sort');
+
+
+  //         if ((parseInt(pep1) > parseInt(pep2)) && (ascending==true)) {
+            
+  //           shouldSwitch = true;
+  //           break;
+  //         }
+          
+  //         if ((parseInt(pep1) < parseInt(pep2)) && (ascending==false)) {
+            
+  //           shouldSwitch = true;
+  //           break;
+  //         }
+  //       }
+
+  //     if (shouldSwitch) {
+  //       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+  //       switching = true;
+  //     }
+  //   }
+  // }
+
+function sortTable(ascending, indx) {
+      var rows, i, x, y, shouldSwitch;
+      var table = document.getElementById("sauceTable");
+      var switching = true;
+      while (switching) {
+       
+        switching = false;
+        rows = table.getElementsByTagName("TR");
+        for (i = 1; i < (rows.length - 1); i++) {
+          shouldSwitch = false;
+         
+          x = rows[i].getElementsByTagName("TD")[indx];
+          y = rows[i + 1].getElementsByTagName("TD")[indx];
+
+          if ((x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) && (ascending == true)) {
+       
+            shouldSwitch= true;
+            break;
+          }
+        
+          
+          if ((x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) && (ascending == false)) {
+       
+            shouldSwitch= true;
+            break;
+          }
+        }
+      if (shouldSwitch) {
+        
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
+  }
+
+
+
+  // on-click sort function calls
+  $("#asc-sauce-sort").click(function() {
+    sortTable(true, 0)
   });
+
+  $("#desc-sauce-sort").click(function() {
+    sortTable(false, 0)
+  });
+
+
+  $("#asc-pepper-sort").click(function() {
+    sortTable(true, 1)
+  });
+
+  $("#desc-pepper-sort").click(function() {
+    sortTable(false, 1)
+  });
+
+
+  $("#asc-scoville-sort").click(function() {
+    sortTablebyNum(true)
+  });
+
+  $("#desc-scoville-sort").click(function() {
+    sortTablebyNum(false)
+  });
+
+
+   $("#asc-origin-sort").click(function() {
+    sortTable(true, 3)
+  });
+
+  $("#desc-origin-sort").click(function() {
+    sortTable(false, 3)
+  });
+
+
+   $("#asc-site-sort").click(function() {
+    sortTablebyNum(true)
+  });
+
+  $("#desc-site-sort").click(function() {
+    sortTablebyNum(false)
+  });
+ 
